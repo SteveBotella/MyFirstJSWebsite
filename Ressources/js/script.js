8f87b70cef90dc9429c89218ddad3067119d8de9
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-03-26 14:25:12 +0100

    fix: "for agent" as a function

--- Ressources/js/script.js
+++ Ressources/js/script.js
@@ -13,36 +13,8 @@ $(document).ready(function(){
 
     .done(function(response){
         console.log(response)
-        // For each valAgent        
-        for (let agentNum = 0; agentNum < response.data.length; agentNum++){
-            // Create new div
-            let newDiv = document.createElement("div");
-            
-            // Create object
-            let agent = new Object();
-            agent = {
-                name: response.data[agentNum].displayName,
-                icon: response.data[agentNum].displayIcon,
-                description: response.data[agentNum].description,
-                id: agentNum,
-            }             
-            console.log(agent);
-            // Create img element & set src from agent.icon (image url)
-            let agentIcon = document.createElement("img");
-            agentIcon.setAttribute("src", agent.icon);
-            
-            // Inside the new created div, append all valAgent informations
-            newDiv.append(agent.name);
-            newDiv.append(agentIcon);
-            newDiv.append(agent.description);
-
-            // Set class & unique id for the new created div
-            newDiv.setAttribute("class", "DivAgent");
-            newDiv.setAttribute("id", "Div" + agentNum);
-
-            // Append the new div to the parent valAgents div
-            $("#valAgents").append(newDiv);                                   
-        }        
+        // For each valAgent
+        addAgent(response);        
     })
     
     .fail(function(error){
@@ -54,3 +26,35 @@ $(document).ready(function(){
     });
     */   
 });
+
+function addAgent (response) {
+    for (let agentNum = 0; agentNum < response.data.length; agentNum++){
+        // Create new div
+        let newDiv = document.createElement("div");
+        
+        // Create object
+        let agent = new Object();
+        agent = {
+            name: response.data[agentNum].displayName,
+            icon: response.data[agentNum].displayIcon,
+            description: response.data[agentNum].description,
+            id: agentNum,
+        }             
+        console.log(agent);
+        // Create img element & set src from agent.icon (image url)
+        let agentIcon = document.createElement("img");
+        agentIcon.setAttribute("src", agent.icon);
+        
+        // Inside the new created div, append all valAgent informations
+        newDiv.append(agent.name);
+        newDiv.append(agentIcon);
+        newDiv.append(agent.description);
+
+        // Set class & unique id for the new created div
+        newDiv.setAttribute("class", "DivAgent");
+        newDiv.setAttribute("id", "Div" + agentNum);
+
+        // Append the new div to the parent valAgents div
+        $("#valAgents").append(newDiv);                                   
+    }
+}
