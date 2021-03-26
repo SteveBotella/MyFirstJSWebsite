6f984fd1c7bed6c760f0c68a00ca66a49d8da13a
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-03-26 14:00:15 +0100

    feat: Feed ready

--- Ressources/js/script.js
+++ Ressources/js/script.js
@@ -1,34 +1,47 @@
-// Feed 
-
+// Feed valAgents
 $(document).ready(function(){
     $.ajax({
-        // Url de la requête
+        // Request Url
         url: "https://valorant-api.com/v1/agents",
 
-        // Méthode d'envoi (type de requête)
+        // send methode (Request type)
         method: "GET",
 
-        // Format de réponse attendu
+        // answer format
         dataType : "json",
     })
 
     .done(function(response){
-        console.log(response)        
-        for (let agentNum = 0; agentNum < response.data.length; agentNum++){ 
+        console.log(response)
+        // For each valAgent        
+        for (let agentNum = 0; agentNum < response.data.length; agentNum++){
+            // Create new div
+            let newDiv = document.createElement("div");
+            
+            // Create object
             let agent = new Object();
             agent = {
-            name: response.data[agentNum].displayName,
-            icon: response.data[agentNum].displayIcon,
-            description: response.data[agentNum].description,
-            id: agentNum,
-        }             
-            console.log(agent); 
-            
-            $("#valAgents").append(agent.name);
+                name: response.data[agentNum].displayName,
+                icon: response.data[agentNum].displayIcon,
+                description: response.data[agentNum].description,
+                id: agentNum,
+            }             
+            console.log(agent);
+            // Create img element & set src from agent.icon (image url)
             let agentIcon = document.createElement("img");
-            agentIcon.setAttribute("src", agent.icon);            
-            $("#valAgents").append(agentIcon);
-            $("#valAgents").append(agent.description);            
+            agentIcon.setAttribute("src", agent.icon);
+            
+            // Inside the new created div, append all valAgent informations
+            newDiv.append(agent.name);
+            newDiv.append(agentIcon);
+            newDiv.append(agent.description);
+
+            // Set class & unique id for the new created div
+            newDiv.setAttribute("class", "DivAgent");
+            newDiv.setAttribute("id", "Div" + agentNum);
+
+            // Append the new div to the parent valAgents div
+            $("#valAgents").append(newDiv);                                   
         }        
     })
     
