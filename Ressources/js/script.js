600f794039ca6b7aea3215d4b3bbe2f7e0c21ff6
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-03-26 17:09:07 +0100

    feat: Add API for maps

--- Ressources/js/script.js
+++ Ressources/js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
         */   
     }
 
-    function addAgent (response) {
+    function addAgent(response) {
         for (let agentNum = 0; agentNum < response.data.length; agentNum++){
             // Create new div
             let newDiv = document.createElement("div");
@@ -58,7 +58,38 @@ $(document).ready(function(){
         }
     }
 
-    function addWeapon (response) {
+    function addWeapon(response) {
+        for (let agentNum = 0; agentNum < response.data.length; agentNum++){
+            // Create new div
+            let newDiv = document.createElement("div");
+            
+            // Create object
+            let agent = {
+                name: response.data[agentNum].displayName,
+                icon: response.data[agentNum].displayIcon,
+                description: response.data[agentNum].description,
+                id: agentNum,
+            }
+    
+            console.log(agent);
+            // Create img element & set src from agent.icon (image url)
+            let agentIcon = document.createElement("img");
+            agentIcon.setAttribute("src", agent.icon);
+            
+            // Inside the new created div, append all valAgent informations
+            newDiv.append(agent.name);
+            newDiv.append(agentIcon);
+    
+            // Set class & unique id for the new created div
+            newDiv.setAttribute("class", "DivAgent");
+            newDiv.setAttribute("id", "Div" + agentNum);
+    
+            // Append the new div to the parent valAgents div
+            $("#valAgents").append(newDiv);                                   
+        }
+    }
+
+    function addMap(response) {
         for (let agentNum = 0; agentNum < response.data.length; agentNum++){
             // Create new div
             let newDiv = document.createElement("div");
@@ -121,6 +152,16 @@ $(document).ready(function(){
             "https://valorant-api.com/v1/weapons",
             'GET', 
             (res) =>  addWeapon(res),
+            (error) =>  alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error))
+        )
+    });
+
+    $('#displayMaps').click(event => {
+        clearChildren();
+        requestApi(
+            "https://valorant-api.com/v1/maps",
+            'GET', 
+            (res) =>  addMap(res),
             (error) =>  alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error))
         )
     });
