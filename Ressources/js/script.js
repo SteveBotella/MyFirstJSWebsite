936e5762b0a7fb3db60fdad6c35e5a336514a0c1
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-03-26 16:07:57 +0100

    wip : Click on buttons add feeds

--- Ressources/js/script.js
+++ Ressources/js/script.js
@@ -1,60 +1,124 @@
 // Feed valAgents
 $(document).ready(function(){
-    $.ajax({
-        // Request Url
-        url: "https://valorant-api.com/v1/agents",
-
-        // send methode (Request type)
-        method: "GET",
-
-        // answer format
-        dataType : "json",
-    })
-
-    .done(function(response){
-        console.log(response)
-        // For each valAgent
-        addAgent(response);        
-    })
+    function requestApi(url, method, onSuccess, onFail) {
+        $.ajax({
+            // Request Url
+            url,
     
-    .fail(function(error){
-        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
-    /*
-    .always(function(){
-        alert("Requête effectuée");
-    });
-    */   
-});
-
-function addAgent (response) {
-    for (let agentNum = 0; agentNum < response.data.length; agentNum++){
-        // Create new div
-        let newDiv = document.createElement("div");
+            // send methode (Request type)
+            method,
+    
+            // answer format
+            dataType : "json",
+        })
+    
+        .done(function(response){
+            onSuccess(response)
+        })
         
-        // Create object
-        let agent = new Object();
-        agent = {
-            name: response.data[agentNum].displayName,
-            icon: response.data[agentNum].displayIcon,
-            description: response.data[agentNum].description,
-            id: agentNum,
-        }             
-        console.log(agent);
-        // Create img element & set src from agent.icon (image url)
-        let agentIcon = document.createElement("img");
-        agentIcon.setAttribute("src", agent.icon);
-        
-        // Inside the new created div, append all valAgent informations
-        newDiv.append(agent.name);
-        newDiv.append(agentIcon);
-        newDiv.append(agent.description);
-
-        // Set class & unique id for the new created div
-        newDiv.setAttribute("class", "DivAgent");
-        newDiv.setAttribute("id", "Div" + agentNum);
-
-        // Append the new div to the parent valAgents div
-        $("#valAgents").append(newDiv);                                   
+        .fail(function(error){
+            onFail(error);
+        })
+        /*
+        .always(function(){
+            alert("Requête effectuée");
+        });
+        */   
     }
-}
+
+    function addAgent (response) {
+        for (let agentNum = 0; agentNum < response.data.length; agentNum++){
+            // Create new div
+            let newDiv = document.createElement("div");
+            
+            // Create object
+            let agent = {
+                name: response.data[agentNum].displayName,
+                icon: response.data[agentNum].displayIcon,
+                description: response.data[agentNum].description,
+                id: agentNum,
+            }
+    
+            console.log(agent);
+            // Create img element & set src from agent.icon (image url)
+            let agentIcon = document.createElement("img");
+            agentIcon.setAttribute("src", agent.icon);
+            
+            // Inside the new created div, append all valAgent informations
+            newDiv.append(agent.name);
+            newDiv.append(agentIcon);
+            newDiv.append(agent.description);
+    
+            // Set class & unique id for the new created div
+            newDiv.setAttribute("class", "DivAgent");
+            newDiv.setAttribute("id", "Div" + agentNum);
+    
+            // Append the new div to the parent valAgents div
+            $("#valAgents").append(newDiv);                                   
+        }
+    }
+
+    function addWeapon (response) {
+        for (let agentNum = 0; agentNum < response.data.length; agentNum++){
+            // Create new div
+            let newDiv = document.createElement("div");
+            
+            // Create object
+            let agent = {
+                name: response.data[agentNum].displayName,
+                icon: response.data[agentNum].displayIcon,
+                description: response.data[agentNum].description,
+                id: agentNum,
+            }
+    
+            console.log(agent);
+            // Create img element & set src from agent.icon (image url)
+            let agentIcon = document.createElement("img");
+            agentIcon.setAttribute("src", agent.icon);
+            
+            // Inside the new created div, append all valAgent informations
+            newDiv.append(agent.name);
+            newDiv.append(agentIcon);
+            newDiv.append(agent.description);
+    
+            // Set class & unique id for the new created div
+            newDiv.setAttribute("class", "DivAgent");
+            newDiv.setAttribute("id", "Div" + agentNum);
+    
+            // Append the new div to the parent valAgents div
+            $("#valAgents").append(newDiv);                                   
+        }
+    }
+
+
+    function onPageLoad() {
+        requestApi(
+            "https://valorant-api.com/v1/agents",
+            'GET', 
+            (res) =>  addAgent(res),
+            (error) =>  alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error))
+        );
+    }
+
+    // --- EVENTS ---
+    $('#displayWeapons').click(event => {
+        requestApi(
+            "https://valorant-api.com/v1/weapons",
+            'GET', 
+            (res) =>  addWeapon(res),
+            (error) =>  alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error))
+        )
+    });
+
+    $('#displayAgents').click(event => {
+        requestApi(
+            "https://valorant-api.com/v1/agents",
+            'GET', 
+            (res) =>  addAgent(res),
+            (error) =>  alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error))
+        )
+    });
+
+    onPageLoad();
+
+});
