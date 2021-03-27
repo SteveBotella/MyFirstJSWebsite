c4b5a9333e466f314d7b1e68756bfac5ab08efc5
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-03-27 22:04:41 +0100

    wip: Add character with input (keyListener)

--- Ressources/js/script.js
+++ Ressources/js/script.js
@@ -202,19 +202,109 @@ $(document).ready(function(){
     onPageLoad();
 
     // --- GAME ---
-    let context, controller, rectangle, loop;
+    let context, controller, playerCharacter, loop;
 
     context = document.querySelector("canvas").getContext("2d");
 
     context.canvas.height = 600;
     context.canvas.width = 1080;
 
+    // Add image as BG to the scene
     let background = new Image();
     background.src = "Ressources/images/canvasBG.png";
 
-    // Make sure the image is loaded first otherwise nothing will draw.
+    let characterViper = new Image();
+    characterViper.src = "Ressources/images/characterViper.png";
+
+    // Make sure the image is loaded
     background.onload = function(){
-        context.drawImage(background,0,0);   
+        context.drawImage(background, 0, 0);   
     }
+    
+    characterViper.onload = function(){
+        context.drawImage(characterViper, 200, 200);   
+    }
+
+    // playerCharacter
+    playerCharacter = {
+        height: 192,
+        width: 96,
+        jumping: false,
+        x: 300, // Center of canvas
+        x_velocity: 0,
+        y: 300,
+        y_velocity: 0
+    };
+  
+    controller = {
+        left:false,
+        right:false,
+        up:false,        
+    };
+
+    loop = function() {
+        if (controller.up && playerCharacter.jumping == false) {
+            playerCharacter.y_velocity -= 20;
+            playerCharacter.jumping = true;
+        }
+
+        if (controller.left == true) {
+            playerCharacter.x_velocity -= 0.5;            
+        }
+
+        if (controller.right == true) {
+            playerCharacter.x_velocity += 0.5;
+        }
+
+        playerCharacter.y_velocity += 1.5; //Set gravity
+        playerCharacter.x += playerCharacter.x_velocity; //Move character on the ground with direction
+        playerCharacter.y += playerCharacter.y_velocity; // Falling/jumping
+        playerCharacter.x_velocity *= 0.9; //Friction
+        playerCharacter.y_velocity *= 0.9; //Friction
+
+        // If playerCharacter is falling below floor line
+        if (playerCharacter.y > 400 - 16 - 32) {
+            playerCharacter.jumping = false;
+            playerCharacter.y = 400 - 16 - 32;
+            playerCharacter.y_velocity = 0;
+        }
+        
+        // If playerCharacter is going off the left screen
+        if (playerCharacter.x < - 32) {
+            playerCharacter.x = 1080;            
+        } else if (playerCharacter.x > 1080) { // If playerCharacter is going off the right screen
+            playerCharacter.x = - 32;
+        }
+
+        //context.fillStyle = "#202020";
+        context.fillRect(0, 0, 1080, 600);// x, y, width, height
+        //context.fillStyle = "#ff0000";// hex for red
+        context.drawImage(background, 0, 0);
+        context.drawImage(characterViper, playerCharacter.x, playerCharacter.y);
+        context.beginPath();        
+        //context.rect(playerCharacter.x, playerCharacter.y, playerCharacter.width, playerCharacter.height);
+        //context.fill();
+        context.strokeStyle = "rgba(0, 0, 0, 0)";
+        context.lineWidth = 0;
+        context.beginPath();
+        context.moveTo(0, 500);
+        context.lineTo(1080, 500);
+        context.stroke();                  
+
+        // call update when the browser is ready to draw again
+        window.requestAnimationFrame(loop);
+    };
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === "Numpad5")        controller.up = true
+        else if (e.code === "Numpad3") controller.right = true
+        else if (e.code === "Numpad1") controller.left = true
+    });
+    window.addEventListener("keyup", (e) => {
+        if (e.code === "Numpad5")        controller.up = false
+        else if (e.code === "Numpad3") controller.right = false
+        else if (e.code === "Numpad1") controller.left = false
+    });
+    window.requestAnimationFrame(loop);
 
 });
